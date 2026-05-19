85bcad4411383f4295082b14cf04e351f6681f6d
Kotaro Yamamoto <kotaro.h.yama@gmail.com>
2026-05-19 21:08:40 +0800

    Add: combinations-of-a-phone-number - submission-0

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-0.py
@@ -0,0 +1,33 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if not digits:
+            return []
+        hash_map = {
+            "2": ["a", "b", "c"],
+            "3": ["d", "e", "f"],
+            "4": ["g", "h", "i"],
+            "5": ["j", "k", "l"],
+            "6": ["m", "n", "o"],
+            "7": ["p", "q", "r", "s"],
+            "8": ["t", "u", "v"],
+            "9": ["w", "x", "y", "z"],
+        }
+        result = []
+        path = ""
+
+
+        def helper(i):
+            nonlocal result
+            nonlocal path
+            if i == len(digits):
+                result.append(path)
+                return
+
+            for j in range(len(hash_map[digits[i]])):
+                path = path + hash_map[digits[i]][j]
+                helper(i + 1)
+                path = path[:-1]
+
+        
+        helper(0)
+        return result
