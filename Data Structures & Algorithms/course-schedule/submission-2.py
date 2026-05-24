1e728bd2a55ff237abe7fb0bb08da244d45e50b1
Kotaro Yamamoto <kotaro.h.yama@gmail.com>
2026-05-24 14:21:24 +0800

    Add: course-schedule - submission-2

---
+++ Data Structures & Algorithms/course-schedule/submission-2.py
@@ -0,0 +1,31 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        # Create adj list
+        adj_list = {}
+        for src, dst in prerequisites:
+            if src not in adj_list:
+                adj_list[src] = []
+            if dst not in adj_list:
+                adj_list[dst] = []
+            adj_list[src].append(dst)
+        
+        def dfs(node, adj_list, path):
+            if node in path:
+                return False
+            if not adj_list[node]:
+                return True
+            
+            path.add(node)
+            result = True
+            for neighbor in adj_list[node]:
+                result = result and dfs(neighbor, adj_list, path)
+            path.remove(node)
+            return result
+
+        for course in adj_list:
+            path = set()
+            result = dfs(course, adj_list, path)
+            if not result:
+                return False
+        
+        return True
