4114ca1b2e4f73958b38a4fbb20b8043026d410c
Kotaro Yamamoto <kotaro.h.yama@gmail.com>
2026-05-24 16:30:32 +0800

    Add: course-schedule-ii - submission-8

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-8.py
@@ -0,0 +1,43 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        # Create adj list
+        adj_list = {}
+        for src, dst in prerequisites:
+            if src not in adj_list:
+                adj_list[src] = []
+            if dst not in adj_list:
+                adj_list[dst] = []
+            adj_list[src].append(dst)
+
+        for course in range(numCourses):
+            if course not in adj_list:
+                adj_list[course] = []
+        
+        def dfs(node, path):
+            if not adj_list[node]:
+                if node not in path:
+                    path.append(node)
+                return True
+            if node in visited:
+                return False
+
+            visited.add(node)
+            result = True
+            for neighbor in adj_list[node]:
+                result = result and dfs(neighbor, path)
+            if not result:
+                return False
+            if node not in path:
+                path.append(node)
+            visited.remove(node)
+            return result
+            
+        visited = set()
+        path = []
+        print(adj_list)
+        for course in adj_list:
+            result = dfs(course, path)
+            if not result:
+                return []
+
+        return path
