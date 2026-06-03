2a23ba6a3a5ffa1cacacbad29efc7e779ff2768b
Kotaro Yamamoto <kotaro.h.yama@gmail.com>
2026-06-03 13:07:20 +0800

    Add: buy-and-sell-crypto-with-cooldown - submission-3

---
+++ Data Structures & Algorithms/buy-and-sell-crypto-with-cooldown/submission-3.py
@@ -0,0 +1,24 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+
+        def memoization(i, having, prev_date, cache):
+            key = f"{i}-{having}-{prev_date}"
+            if i == len(prices):
+                return 0
+            if key in cache:
+                return cache[key]
+            
+            if having:
+                profit = prices[i] - prices[prev_date]
+                cache[key] = max(profit + memoization(i + 1, False, i, cache),
+                               memoization(i + 1, having, prev_date, cache))
+            else:
+                if prev_date == i - 1 and prev_date != -1:
+                    cache[key] = memoization(i + 1, having, prev_date, cache)
+                else:
+                    cache[key] = max(memoization(i + 1, True, i, cache),
+                                   memoization(i + 1, having, prev_date, cache))
+            return cache[key]
+        
+        memo = {}
+        return memoization(0, False, -1, memo)
