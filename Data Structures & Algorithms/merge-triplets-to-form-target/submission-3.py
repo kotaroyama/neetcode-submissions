1c0de0f17050079999c21dc89d796c6e3b8b04bc
Kotaro Yamamoto <kotaro.h.yama@gmail.com>
2026-06-09 15:35:12 +0800

    Add: merge-triplets-to-form-target - submission-3

---
+++ Data Structures & Algorithms/merge-triplets-to-form-target/submission-3.py
@@ -0,0 +1,32 @@
+class Solution:
+    def mergeTriplets(self, triplets: List[List[int]], target: List[int]) -> bool:
+        if len(triplets) == 1:
+            return triplets[0] == target
+        
+        hash_map = {
+            target[0]: 0,
+            target[1]: 0,
+            target[2]: 0,
+        }
+
+        for triplet in triplets:
+            valid = True
+            for i in range(3):
+                if triplet[i] > target[i]:
+                    valid = False
+                    break
+
+            if valid:
+                if triplet[0] == target[0]:
+                    hash_map[target[0]] = 1
+                if triplet[1] == target[1]:
+                    hash_map[target[1]] = 1
+                if triplet[2] == target[2]:
+                    hash_map[target[2]] = 1
+        
+        if (hash_map[target[0]] == 1 and
+            hash_map[target[1]] == 1 and
+            hash_map[target[2]] == 1):
+            return True
+        
+        return False
