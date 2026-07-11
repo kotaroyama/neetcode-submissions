13d7cd12a59343865335c244bb210ae93a1796f5
Kotaro Yamamoto <kotaro.h.yama@gmail.com>
2026-07-11 19:21:05 +0800

    Add: find-median-in-a-data-stream - submission-2

---
+++ Data Structures & Algorithms/find-median-in-a-data-stream/submission-2.py
@@ -0,0 +1,21 @@
+class MedianFinder:
+
+    def __init__(self):
+        self.array = []
+
+    def addNum(self, num: int) -> None:
+        heapq.heappush_max(self.array, num)
+
+    def findMedian(self) -> float:
+        tmp_array = self.array.copy()
+        orig_len = len(self.array)
+        while len(tmp_array) > orig_len / 2:
+            result = heapq.heappop_max(tmp_array)
+
+        # Even length
+        if orig_len % 2 != 0:
+            return result
+
+        # Odd length
+        result += heapq.heappop_max(tmp_array)
+        return result / 2
