8a40b22f0bf67e5b235746f24d9c8cbaa6ad7da5
Kotaro Yamamoto <kotaro.h.yama@gmail.com>
2026-07-12 15:30:28 +0800

    Add: search-for-word-ii - submission-6

---
+++ Data Structures & Algorithms/search-for-word-ii/submission-6.py
@@ -0,0 +1,32 @@
+class Solution:
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        ROWS, COLS = len(board), len(board[0])
+        results = []
+
+        trie = {}
+        for word in words:
+            node = trie
+            for char in word:
+                node = node.setdefault(char, {})
+            node['#'] = word
+
+        def dfs(i, j, node, seen):
+            if '#' in node:
+                results.append(node.pop('#'))
+            
+            if i < 0 or i >= ROWS or j < 0 or j >= COLS or (i, j) in seen or board[i][j] not in node:
+                return
+            
+            char = board[i][j]
+            seen.add((i, j))
+            dfs(i + 1, j, node[char], seen)
+            dfs(i, j + 1, node[char], seen)
+            dfs(i - 1, j, node[char], seen)
+            dfs(i, j - 1, node[char], seen)
+            seen.remove((i, j))
+        
+        for i in range(ROWS):
+            for j in range(COLS):
+                dfs(i, j, trie, set())
+
+        return results
